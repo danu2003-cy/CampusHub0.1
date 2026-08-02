70da24431e23362cc2970fc12c2eb207d2fca93d
Kaveesha Upathissa <kaveeshaupathissa101@gmail.com>
2026-08-02 13:48:46 +0530

    Implement announcement service

--- backend/src/main/java/com/campushub/dto/AnnouncementDto.java
+++ backend/src/main/java/com/campushub/dto/AnnouncementDto.java
@@ -1,12 +1,10 @@
 package com.campushub.dto;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 /**
  * Data Transfer Object for Announcement.
- * TODO (Member 6): map fields to/from the Announcement entity in the service layer.
  */
 @Data
 @NoArgsConstructor
